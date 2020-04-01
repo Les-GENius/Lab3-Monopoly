5f6131b42cb944b4dedd31915cb6cb138bdf3c88
Doran Kayoumi <dorankayoumi@gmail.com>
2020-04-01 11:04:28 +0200

    add travis test

---
+++ src/test/java/ch/heigvd/gen/labo/TestTest.java
@@ -0,0 +1,13 @@
+package ch.heigvd.gen.labo;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestTest {
+
+    @Test
+    public void test() {
+        assertEquals(true, true);
+    }
+
+}
