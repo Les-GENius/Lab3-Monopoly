49bc331f8b9b31ee464384670446df25e3e66875
Gaetan2907 <gaetan.daubresse@gmail.com>
2020-04-11 10:51:37 +0200

    Test for Die

---
+++ src/test/java/MonopolyTest.java
@@ -0,0 +1,14 @@
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MonopolyTest {
+    @Test
+    public void testDie(){
+        Die die = new Die();
+        for(int i = 0; i < 100; ++i){
+            die.roll();
+            assertTrue((0 <= die.getFaceValue()) && (6 >= die.getFaceValue()));
+        }
+    }
+}
