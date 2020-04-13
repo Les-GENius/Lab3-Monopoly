b491cff219b4da109b17f62bc320d23f0589a321
kurisukun <barroshenriques.chris@gmail.com>
2020-04-13 12:05:12 +0200

    rollTest changed to Repeated test

--- src/test/java/DieTest.java
+++ src/test/java/DieTest.java
@@ -1,19 +1,16 @@
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 public class DieTest {
 
-    @Test
+    @RepeatedTest(50)
     void rollTest(){
         Die die = new Die();
-        boolean valueTest = true;
-        for(int i = 0; i < 50; ++i){
-            die.roll();
-            int value = die.getFaceValue();
-            valueTest = (value >= 1 && value <= 6);
-        }
-        assertTrue(valueTest);
+        die.roll();
+        int value = die.getFaceValue();
+        assertTrue(value >= 1 && value <= 6);
     }
 
 }
