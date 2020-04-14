f776c8b2c02463874d107ffdd19928550848f01e
Doran Kayoumi <dorankayoumi@gmail.com>
2020-04-14 10:26:46 +0200

    Add Piece

--- src/main/java/Piece.java
+++ src/main/java/Piece.java
@@ -1,2 +1,16 @@
 public class Piece {
+    private String name;
+    private Square location;
+
+    public Piece(String name) {
+        this.name = name;
+    }
+
+    public Square getLocation() {
+        return location;
+    }
+
+    public void setLocation(Square location) {
+        this.location = location;
+    }
 }
