96221c72e768ca7493ff98a82b5c941eb870618a
Doran Kayoumi <dorankayoumi@gmail.com>
2020-04-14 14:29:14 +0200

    Update Player to create Piece and add location to Piece constructor

--- src/test/java/PieceTest.java
+++ src/test/java/PieceTest.java
@@ -6,12 +6,8 @@ public class PieceTest {
 
     @Test
     void pieceCanMoveTest() {
-        Square src = new Square("Go");
         Square dest = new Square("Square 1");
-        Piece p = new Piece("Car");
-
-        p.setLocation(src);
-        assertSame(p.getLocation(), src);
+        Piece p = new Piece("Car", Board.FIRST);
 
         p.setLocation(dest);
         assertSame(p.getLocation(), dest);
