a6f22f0a8fe82a9bfd044b3feebaf4b6193e76d1
Doran Kayoumi <dorankayoumi@gmail.com>
2020-04-14 10:26:46 +0200

    Add getter & setter for location

--- src/main/java/Piece.java
+++ src/main/java/Piece.java
@@ -5,4 +5,12 @@ public class Piece {
     public Piece(String name) {
         this.name = name;
     }
+
+    public Square getLocation() {
+        return location;
+    }
+
+    public void setLocation(Square location) {
+        this.location = location;
+    }
 }
