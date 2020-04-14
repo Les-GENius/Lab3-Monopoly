7b3be46096f826af8cecb11be1c42f264f5e2cef
kurisukun <barroshenriques.chris@gmail.com>
2020-04-14 13:29:16 +0200

    Board class

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,2 +1,22 @@
+import java.util.ArrayList;
+
 public class Board {
+
+
+    private ArrayList<Square> squares;
+    private static int nbSquares = 40;
+
+    Board(){
+        squares = new ArrayList<>(nbSquares);
+        int i = 0;
+        for(Square s: squares){
+            s = new Square(Integer.toString(i));
+            ++i;
+        }
+    }
+
+
+    public Square getSquare(Square oldLocation, int faceValueTotal){
+        return squares.get(squares.indexOf(oldLocation) + faceValueTotal);
+    }
 }
