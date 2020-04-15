67e7845c98010de926cebfa985013b630b94f1b7
Doran Kayoumi <dorankayoumi@gmail.com>
2020-04-15 10:47:18 +0200

    Add setup method for common data

--- src/test/java/PlayerTest.java
+++ src/test/java/PlayerTest.java
@@ -1,19 +1,26 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class PlayerTest {
 
+    Die[] dies;
+    Board board;
+
+    @BeforeEach
+    void setUp() {
+        this.dies = new Die[]{new Die(), new Die()};
+        this.board = new Board();
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"Henry", "Paul", "Colette", "Rémi"})
     void playerTest(String name){
-        Die[] dies = new Die[]{new Die(), new Die()}; 
-
-        Player player = new Player(name, dies, new Board(), MonopolyGame.PieceNames.Car);
+        Player player = new Player(name, this.dies, this.board, MonopolyGame.PieceNames.Car);
         player.takeTurn();
         assertNotNull(player);
     }
